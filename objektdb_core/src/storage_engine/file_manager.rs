f7a9ba53a9baf97605f3af8b6f4fb7e55b350645
AresRebatto <aresrebatt@gmail.com>
2025-06-03 21:13:57 +0200

    first commit

---
+++ objektdb_core/src/storage_engine/file_manager.rs
@@ -0,0 +1,3 @@
+pub fn stampa() {
+    println!("Hello, world!");
+}
---
+++ objektdb_macros/src/lib.rs
@@ -0,0 +1,91 @@
+mod traits;
+#[doc(inline)]
+use helper::*;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, ItemStruct, LitStr};
+
+#[proc_macro_attribute]
+pub fn objekto_impl(_attr: TokenStream, _item: TokenStream) -> TokenStream {
+    
+    _item
+}
+
+///It should be inserted on top of your structs to declare that it is an entity whose 
+/// instances you want to store. The database in which you want to store the data should 
+/// be specified.
+///If the specified database does not exist, it is automatically created: so if you mistype 
+/// the database name and enter one that does not exist, a new one will automatically be 
+/// created. For the moment , since a language for interacting with data directly is not yet 
+/// available, to delete a database you can make use of the macro `delete_db!(“database_name”)`.
+/// 
+/// In addition, it also implements CRUD trait to perform transactions on the database.
+/// In particular, it implements the following functions:
+/// - `select()`
+/// - `update()`
+/// - `where()`
+/// - `delete()`
+/// # Example
+/// ```
+/// use objektoDB::*;
+/// #[objekto("my_database.db")]
+/// struct Person {
+///    name: String,
+///    age: u32,
+/// }
+#[proc_macro_attribute]
+pub fn objekto(_attr: TokenStream, _item    : TokenStream) -> TokenStream {
+    let input = parse_macro_input!(_item as ItemStruct);
+    let struct_name = &input.ident;
+
+     let db_name_lit = parse_macro_input!(_attr as LitStr);
+    let db_name = db_name_lit.value();
+
+
+    //Qui sarà necessario chiamare la funzione che crea il database
+
+
+    let expanded = quote::quote! {
+        #input
+
+        impl traits::CRUD for #struct_name {
+            //da rivedere
+            fn select() -> Vec<Self> {
+                helper::select::<#struct_name>(&#db_name)
+            }
+
+            fn save(&self) -> Result<(), String> {
+                helper::save(self, &#db_name)
+            }
+
+            fn filter<F>(&self, condition: F) -> Vec<Self>
+            where
+                F: Fn(&Self) -> bool,
+            {
+                helper::filter(self, condition)
+            }
+
+            fn delete(&self) -> Result<(), String> {
+                helper::delete(self, &#db_name)
+            }
+        }
+
+    };
+
+    TokenStream::from(expanded)
+}
+
+///Allows you to delete a database by passing it the name of the database as a parameter.
+///# Example
+/// ```
+/// use objektoDB::*;
+/// 
+/// delete_db!("my_database.db");
+///```
+#[proc_macro]
+pub fn delete_db(input: TokenStream) -> TokenStream {
+    let db_name = input.to_string();
+    //Ancora da mettere in piedi
+    //helper::delete_db(db_name);
+    TokenStream::new()
+}
