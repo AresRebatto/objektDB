2621213103e8f378f0e1a023a122c521e0768887
AresRebatto <aresrebatt@gmail.com>
2025-07-12 21:25:08 +0200

    rewrite partially method for creating db(the old version of that method didn't pass the tests)

--- objektdb_core/src/storage_engine/file_manager.rs
+++ objektdb_core/src/storage_engine/file_manager.rs
@@ -1,6 +1,11 @@
+use std::f32::consts::E;
+use std::fmt::format;
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
-use std::path::{self, Path};
+use std::path::{self, Path, PathBuf};
+
+//DA RIVEDERE TUTTA LA DOCUMENTAZIONE IN TESTA AI METODI
+
 
 /// Magic number that identifies the custom database file format.
 ///
@@ -16,7 +21,7 @@ use std::path::{self, Path};
 /// - Rejects files that do not match the expected format.
 pub const MAGIC_NUMBER: u32 = 0x4D594442;
 
-///Creates a new database from the name. 
+/// Creates a new database from the name. 
 /// 
 /// It initially creates it empty, and you can then insert tables using the 
 /// `create_table()` method.
@@ -41,17 +46,51 @@ pub const MAGIC_NUMBER: u32 = 0x4D594442;
 /// * `Err(String)` if the file does not exist or if an error occurred during creation.
 /// # Example
 /// ```
-/// use objektoDB::storage_engine::file_manager::create_db;
+/// use objektDB::storage_engine::file_manager::create_db;
 /// 
 /// match create_db(String::from("my_database")) {
 ///     Ok(_) => println!("Database created successfully!"),
 ///     Err(e) => println!("Error creating database: {}", e),
 /// }
+/// ```
 ///If the conversion is successful, it returns `Ok(())`, otherwise `Err(error)`
-pub fn create_db(db_name : String) -> Result<(), String> {
-    let db_path = format!("{}.db", db_name);
+pub fn create_db(db_name : String) -> Result<(), String>{
+    match std::fs::create_dir(&db_name){
+        Err(e)=> Err(format!("Error creating the database directory: {}", e)),
+        Ok(_)=>{
 
-    if Path::new(&db_path).exists() {
+            //we need to do binding for the life time
+            let path_dir = format!("{}/{}.db", db_name, db_name);
+            let db_path = Path::new(&path_dir);
+            if db_path.exists(){
+                return Err("The database already exists".to_string()); 
+            }
+
+            
+            match File::create(db_path){
+                Err(e) => {
+                    Err(format!("Error creating db: {}", e))
+
+                    //In caso di annullamento, si deve vedere se esiste ancora
+                    //la directory e, in caso, eliminarla per abortire l'operazione
+                },
+                Ok(_)=>{
+                    return Ok(());
+                }
+            }
+            
+            
+        }
+    }
+}
+/*pub fn create_db(db_name : String) -> Result<(), String> {
+    if let Err(e) = std::fs::create_dir(&db_name) {
+        return Err(format!("Error creating database directory: {}", e));
+    }
+    let db_dir = Path::new(&db_name);
+    let db_path = db_dir.join(format!("{}.db", db_name));
+
+    if db_path.exists() {
         return Err(format!("Database {} already exists", db_name));
     }
 
@@ -59,18 +98,18 @@ pub fn create_db(db_name : String) -> Result<(), String> {
         Ok(mut f) => {
             let mut buffer = Vec::with_capacity(10);
 
-            //header
+            // header
             buffer.extend_from_slice(&MAGIC_NUMBER.to_le_bytes()); // Magic number (4 byte)
-            buffer.extend_from_slice(&[1u8]); // Version(1 byte)
-            buffer.extend_from_slice(&[0u8; 1]); // Number of tables(1 byte)
-            buffer.extend_from_slice(&[0u8; 4]); // flags (for future use)(4 byte)
+            buffer.extend_from_slice(&[1u8]); // Version (1 byte)
+            buffer.extend_from_slice(&[0u8; 1]); // Number of tables (1 byte)
+            buffer.extend_from_slice(&[0u8; 4]); // flags (for future use) (4 byte)
 
             f.write_all(&buffer).map_err(|e| format!("Error writing to the file: {}", e))?;
             Ok(())
         },
         Err(e) => Err(format!("Error in the file creation: {}", e)),
     }
-}
+}*/
 
 /// Creates a new table in the specified database.
 /// 
@@ -97,7 +136,7 @@ pub fn create_db(db_name : String) -> Result<(), String> {
 /// * `Err(String)` if the database file does not exist, is invalid, or if an error occurred during the process.
 /// # Example
 /// ```
-/// use objektoDB::storage_engine::file_manager::create_table;
+/// use objektDB::storage_engine::file_manager::create_table;
 /// match create_table(String::from("my_table"), String::from("my_database")) {
 ///    Ok(_) => println!("Table created successfully!"),
 ///    Err(e) => println!("Error creating table: {}", e),
@@ -106,7 +145,7 @@ pub fn create_db(db_name : String) -> Result<(), String> {
 /// 
 pub fn create_table(_table_name: String, _db_name: String) -> Result<(), String> {
 
-    let path = format!("{}.db", _db_name);
+    let path = format!("{}/{}.db", _db_name, _db_name);
 
     // Check if the database file exists
     if Path::new(&path).exists() {
@@ -174,7 +213,7 @@ pub fn create_table(_table_name: String, _db_name: String) -> Result<(), String>
 /// # Example
 ///
 /// ```
-/// use objektoDB::storage_engine::file_manager::delete_db;
+/// use objektDB::storage_engine::file_manager::delete_db;
 ///
 /// match delete_db(String::from("my_database")) {
 ///     Ok(_) => println!("Database deleted successfully!"),
@@ -183,7 +222,7 @@ pub fn create_table(_table_name: String, _db_name: String) -> Result<(), String>
 /// ```
 pub fn delete_db(db_name: String) -> Result<(), String> {
     let db_name = db_name;
-    let db_path = format!("{}.db", db_name);
+    let db_path = format!("{}/{}.db", db_name, db_name);
     
     if Path::new(&db_path).exists() {
         std::fs::remove_file(&db_path).map_err(|e| format!("Error deleting database: {}", e))?;
