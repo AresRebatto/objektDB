3f588d409f45dc871c043c2944db913174dc7d75
AresRebatto <aresrebatt@gmail.com>
2025-07-20 15:54:43 +0200

    Remove target folder and update gitignore

--- objektdb_core/target/debug/deps/objektdb_core-03572a8a5d8c4011.d
+++
@@ -1,8 +0,0 @@
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_core\target\debug\deps\libobjektdb_core-03572a8a5d8c4011.rmeta: src\lib.rs src\storage_engine\mod.rs src\storage_engine\file_manager.rs src\tests\mod.rs
-
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_core\target\debug\deps\objektdb_core-03572a8a5d8c4011.d: src\lib.rs src\storage_engine\mod.rs src\storage_engine\file_manager.rs src\tests\mod.rs
-
-src\lib.rs:
-src\storage_engine\mod.rs:
-src\storage_engine\file_manager.rs:
-src\tests\mod.rs:
--- objektdb_core/target/debug/deps/objektdb_core-06da150280a48e22.d
+++
@@ -1,8 +0,0 @@
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_core\target\debug\deps\libobjektdb_core-06da150280a48e22.rmeta: src\lib.rs src\storage_engine\mod.rs src\storage_engine\file_manager.rs src\tests\mod.rs
-
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_core\target\debug\deps\objektdb_core-06da150280a48e22.d: src\lib.rs src\storage_engine\mod.rs src\storage_engine\file_manager.rs src\tests\mod.rs
-
-src\lib.rs:
-src\storage_engine\mod.rs:
-src\storage_engine\file_manager.rs:
-src\tests\mod.rs:
