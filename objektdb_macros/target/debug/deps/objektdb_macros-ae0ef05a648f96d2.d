6ebdef56f155e4da4cddd34ae4310447c4d78f72
AresRebatto <aresrebatt@gmail.com>
2025-07-26 23:25:12 +0200

    Remove cached target folder from objektdb_macros

--- objektdb_macros/target/debug/deps/objektdb_macros-ae0ef05a648f96d2.d
+++
@@ -1,7 +0,0 @@
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_macros\target\debug\deps\libobjektdb_macros-ae0ef05a648f96d2.rmeta: src\lib.rs src\traits\mod.rs src\traits\crud.rs
-
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_macros\target\debug\deps\objektdb_macros-ae0ef05a648f96d2.d: src\lib.rs src\traits\mod.rs src\traits\crud.rs
-
-src\lib.rs:
-src\traits\mod.rs:
-src\traits\crud.rs:
--- objektdb_macros/target/debug/deps/objektdb_macros-df45662745b2407f.d
+++
@@ -1,7 +0,0 @@
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_macros\target\debug\deps\libobjektdb_macros-df45662745b2407f.rmeta: src\lib.rs src\traits\mod.rs src\traits\crud.rs
-
-C:\Users\aresr\Documents\Informatica\Rust\objektDB\objektdb_macros\target\debug\deps\objektdb_macros-df45662745b2407f.d: src\lib.rs src\traits\mod.rs src\traits\crud.rs
-
-src\lib.rs:
-src\traits\mod.rs:
-src\traits\crud.rs:
