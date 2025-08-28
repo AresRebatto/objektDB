f9b14f1a88e411c9277cd79aaf7d005a79dd8e18
AresRebatto <aresrebatt@gmail.com>
2025-08-28 14:44:33 +0200

    impl unwrap for FieldType

--- objektdb_core/src/support_mods/field.rs
+++ objektdb_core/src/support_mods/field.rs
@@ -7,13 +7,30 @@
 pub struct Field{
     pub name: String,
     pub is_OID: bool,
-    pub is_FK: bool,
-    pub type_: String //FK types will be managed by a function that will create a kind of register in a
-    //different rust file where will be create a match pattern for convert fk values in effective values
+    
 }
 
-pub enum FieldType{
-    OID,
-    ForeignKey{reference: String},
-    Primitive(String)
+pub enum FieldType<T>{
+    OID(i32),
+    Primitive(T)
+}
+
+
+impl<T> FieldType<T>{
+    pub fn unwrap_oid(&self)-> i32{
+        match self{
+            FieldType::OID(id)=> id.clone(),
+            FieldType::Primitive(_)=>panic!("If you want to unwrap a primitive, you should use the unwrap_primitive() method.")
+        }
+    }
+
+    pub fn unwrap_primitive(&self)-> T
+    where
+        T: Clone
+    {
+    match self{
+        FieldType::OID(_)=> panic!("If you want to unwrap a oid, you should use the unwrap_oid() method."),
+        FieldType::Primitive(val)=>val.clone()
+    }
+    }
 }
